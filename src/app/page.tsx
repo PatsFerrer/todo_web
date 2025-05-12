b6cb019248bd057fe5c9b5d66e70986ba13eead2
Patricia Ferrer <patricia.ferreira@esx.com.br>
2025-05-12 16:19:37 -0300

    refact: redireciona pagina inicial para page de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import { ROUTES } from "@/constants";
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return <div>Hola</div>;
+  return redirect(ROUTES.LOGIN);
 }
